aabaff1e3ee8f613ea896eadccfdc8e8c83122d7
kevinpotosi <kevinpotosi12ab4gmail.com>
2023-12-08 04:03:01 -0500

    limpieza

--- inventario/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-
-def listproducts(request):
-    products = Product.objects.all()
-    print(products)
-    return render(request,'listproducts.html', {'products' : products})
-
