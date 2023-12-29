8d375c287e735605ce46e3b72bb846045e3f18db
kevinpotosi <kevinpotosi12ab4gmail.com>
2023-12-29 03:05:32 -0500

    render

--- moduloinventario/settings.py
+++ moduloinventario/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tnuy^5d%aj9adi1y^-dz=r11f^8bagjzysygl+y=a!h5yuk)8='
+# SECRET_KEY = 'django-insecure-tnuy^5d%aj9adi1y^-dz=r11f^8bagjzysygl+y=a!h5yuk)8='
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -78,14 +85,19 @@ WSGI_APPLICATION = 'moduloinventario.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Inventario',
-        'USER': 'postgres',
-        'PASSWORD':'root',
-        'HOST': 'localhost',
-        'PORT': '5433'
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'Inventario',
+    #     'USER': 'postgres',
+    #     'PASSWORD':'root',
+    #     'HOST': 'localhost',
+    #     'PORT': '5433'
+    # }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:root@localhost:5433/Inventario',
+        conn_max_age=600
+    )
 }
 
 
