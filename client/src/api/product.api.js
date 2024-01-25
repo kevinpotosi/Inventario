4dd75266168b83213b725272bd3f58d7fb437475
kevinpotosi <kevinpotosi12ab4gmail.com>
2024-01-25 01:36:37 -0500

    correccion

--- client/src/api/product.api.js
+++ client/src/api/product.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const productApi = axios.create({
-    baseURL: 'http://localhost:8000/inventario/products/'
+    baseURL: 'https://inventario-phue.onrender.com/inventario/products/'
 });
 
 export const getAllProduct = () => productApi.get('/');
