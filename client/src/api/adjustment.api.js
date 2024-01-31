3f4dbff61820b1cf46e71fad3891264d222b33c7
kevinpotosi <kevinpotosi12ab4gmail.com>
2024-01-31 08:04:05 -0500

    ajustes

--- client/src/api/adjustment.api.js
+++ client/src/api/adjustment.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const adjustmentApi = axios.create({
-    baseURL: 'http://127.0.0.1:8000/inventario/adjustment/'
+    baseURL: 'https://inventario-phue.onrender.com/inventario/adjustment/'
 })
 
 export const getAllAdjustment = () => adjustmentApi.get('/')
