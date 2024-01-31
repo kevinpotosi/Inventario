40615debb4f8adc69555c8edc307e6b62207617f
kevinpotosi <kevinpotosi12ab4gmail.com>
2024-01-31 08:19:40 -0500

    cambio rutas

--- client/src/pages/adjustmentpage.jsx
+++ client/src/pages/adjustmentpage.jsx
@@ -19,7 +19,7 @@ export function Adjustmentpage() {
                     INVENTARIO
                   </a>
                   <a
-                    href="/adjustment"
+                    href="../pages/adjustmentpage.jsx"
                     class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                   >
                     Ajustes
